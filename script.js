135a3d535ac595470f1b8e7b52c55a7f2f415338
NICOLA-200 <fatepepe66@gmail.com>
2023-05-16 11:30:38 +0200

    added full javascript

--- script.js
+++ script.js
@@ -40,4 +40,16 @@ let weather = {
         },
       };
 
-     
+      document.querySelector(".search button").addEventListener("click", function () {
+          weather.search();
+        });
+        
+        document
+          .querySelector(".search-bar")
+          .addEventListener("keyup", function (event) {
+            if (event.key == "Enter") {
+              weather.search();
+            }
+          });
+        
+        weather.fetchWeather("Denver");
